a2d35f9aabc2671097de46d087b78d4f00bb21f3
Siarhei_Lisiunin <siarhei_lisiunin@epam.com>
2025-01-07 00:00:20 +0100

    message5

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,22 +1,21 @@
 exports.handler = async (event) => {
-    // const path = event.http.path;
-    // const method = event.http.method;
+    const path = event.requestContext.http.path;
+    const method = event.requestContext.http.method;
 
-    return { ...event };
     // TODO implement
-    // if (path === "/hello" && method === "GET") {
-    //     const response = {
-    //         statusCode: 200,
-    //         message: JSON.stringify('Hello from Lambda'),
-    //     };
-    //
-    //     return response;
-    // } else {
-    //     const unhappyResponse = {
-    //         statusCode: 400,
-    //         message: JSON.stringify(`Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`),
-    //     }
-    //
-    //     return unhappyResponse;
-    // }
+    if (path === "/hello" && method === "GET") {
+        const response = {
+            statusCode: 200,
+            message: JSON.stringify('Hello from Lambda'),
+        };
+
+        return response;
+    } else {
+        const unhappyResponse = {
+            statusCode: 400,
+            message: JSON.stringify(`Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`),
+        }
+
+        return unhappyResponse;
+    }
 };
